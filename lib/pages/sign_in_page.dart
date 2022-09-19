d1e8df9a8a6ed0bbc554c689983b71391df518d9
kawarabata <kwrbt0404@gmail.com>
2022-09-19 22:09:37 +0900

    Googleでログインできるようにする

---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:itsukaji_flutter/pages/task_list_page.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            try {
+              final userCredential = await signInWithGoogle();
+              if (!mounted) return;
+
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const TaskListPage()),
+              );
+            } on Exception catch (e) {
+              print('${e.toString()}');
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  backgroundColor: Colors.black,
+                  content: Text(
+                    'ログインに失敗しました。時間をおいて再度お試しください。',
+                    style: TextStyle(color: Colors.redAccent, letterSpacing: 0.5),
+                  ),
+                ),
+              );
+            }
+          },
+          child: const Text('Sign In'),
+        ),
+      ),
+    );
+  }
+}
+
+// Googleを使ってサインイン
+Future<UserCredential> signInWithGoogle() async {
+  // 認証フローのトリガー
+  final googleUser = await GoogleSignIn(scopes: ['email']).signIn();
+  // リクエストから、認証情報を取得
+  final googleAuth = await googleUser!.authentication;
+  // クレデンシャルを新しく作成
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+  // サインインしたら、UserCredentialを返す
+  return FirebaseAuth.instance.signInWithCredential(credential);
+}
