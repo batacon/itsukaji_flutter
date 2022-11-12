1faed87a9889fe10b953814a87359da856c7a55a
kawarabata <kwrbt0404@gmail.com>
2022-11-12 21:21:16 +0900

    svgを表示できるようにする

--- lib/components/task_card.dart
+++ lib/components/task_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:itsukaji_flutter/models/task.dart';
 
 class TaskCard extends StatelessWidget {
@@ -18,27 +19,37 @@ class TaskCard extends StatelessWidget {
             Text(
               'every ${task.intervalDays} days',
               style: const TextStyle(
-                color: Color(0xFF333333),
                 fontSize: 12,
                 decoration: TextDecoration.underline,
               ),
             ),
-            const SizedBox(height: 4.0),
+            const SizedBox(height: 4),
             Text(
               task.name,
               style: const TextStyle(
-                color: Color(0xFF333333),
                 fontSize: 16,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            const SizedBox(height: 4.0),
+            const SizedBox(height: 4),
             Row(
               children: [
-                const SizedBox(width: 16.0),
-                Text('あと${task.daysUntilNext()} 日'),
-                const SizedBox(width: 8.0),
-                Text('前回 ${task.lastDoneDateFormatted()}'),
+                const SizedBox(width: 16),
+                SvgPicture.asset(
+                  'assets/icons/icon_bell_ring.svg',
+                  width: 16,
+                  height: 16,
+                ),
+                const SizedBox(width: 4),
+                Text('あと${task.daysUntilNext()} 日', style: const TextStyle(fontSize: 14)),
+                const SizedBox(width: 8),
+                SvgPicture.asset(
+                  'assets/icons/icon_calendar_check.svg',
+                  width: 16,
+                  height: 16,
+                ),
+                const SizedBox(width: 4),
+                Text('前回 ${task.lastDoneDateFormatted()}', style: const TextStyle(fontSize: 14)),
               ],
             )
           ],
