a31a472e7f957eba45c26028835267f230e8d46c
kawarabata <kwrbt0404@gmail.com>
2023-04-23 14:47:23 +0900

    QRコードを別ページで表示する

--- lib/presentation/pages/group_settings_page.dart
+++ lib/presentation/pages/group_settings_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:itsukaji_flutter/common/show_snack_bar_with_text.dart';
 import 'package:itsukaji_flutter/models/group.dart';
 import 'package:itsukaji_flutter/presentation/components/member_list_section.dart';
+import 'package:itsukaji_flutter/presentation/pages/qr_code_invitation_page.dart';
 import 'package:itsukaji_flutter/presentation/providers/current_group_provider.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class GroupSettingsPage extends ConsumerWidget {
   const GroupSettingsPage({super.key});
@@ -46,52 +45,12 @@ class GroupSettingsPage extends ConsumerWidget {
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         ),
         onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) {
-              return _buildQRCodeSection(context, ref, currentGroup);
-            },
-          );
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return const QRCodeInvitationPage();
+          }));
         },
         child: const Text('メンバーを招待する', style: TextStyle(fontWeight: FontWeight.w700)),
       ),
-    );
-  }
-
-  Widget _buildQRCodeSection(final BuildContext context, final WidgetRef ref, final Group currentGroup) {
-    return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 40),
-          const Text('招待を受けた人が下記のQRコードを'),
-          const Text('スキャンするとグループに参加できます。'),
-          const SizedBox(height: 32),
-          QrImage(
-            data: currentGroup.invitationCode,
-            version: QrVersions.auto,
-            size: 200.0,
-          ),
-          const SizedBox(height: 32),
-          _buildRegenerateInvitationCodeButton(context, ref),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildRegenerateInvitationCodeButton(final BuildContext context, final WidgetRef ref) {
-    return ElevatedButton.icon(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        side: const BorderSide(color: Colors.black),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      ),
-      icon: const Icon(Icons.refresh),
-      onPressed: () {
-        ref.watch(currentGroupProvider.notifier).regenerateInvitationCode();
-        showSnackBarWithText(context, '招待コードを更新しました。');
-      },
-      label: const Text('更新する'),
     );
   }
 }
---
+++ lib/presentation/pages/qr_code_invitation_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:itsukaji_flutter/common/show_snack_bar_with_text.dart';
+import 'package:itsukaji_flutter/presentation/providers/current_group_provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QRCodeInvitationPage extends ConsumerWidget {
+  const QRCodeInvitationPage({super.key});
+
+  @override
+  Widget build(final BuildContext context, final WidgetRef ref) {
+    final currentGroup = ref.watch(currentGroupProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('招待コード'),
+        centerTitle: true,
+      ),
+      body: currentGroup == null
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 40),
+                  const Text('招待を受けた人が下記のQRコードを'),
+                  const Text('スキャンするとグループに参加できます。'),
+                  const SizedBox(height: 32),
+                  QrImage(
+                    data: currentGroup.invitationCode,
+                    version: QrVersions.auto,
+                    size: 200.0,
+                  ),
+                  const SizedBox(height: 32),
+                  _buildRegenerateInvitationCodeButton(context, ref),
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget _buildRegenerateInvitationCodeButton(final BuildContext context, final WidgetRef ref) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        side: const BorderSide(color: Colors.black),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      ),
+      icon: const Icon(Icons.refresh),
+      onPressed: () {
+        ref.watch(currentGroupProvider.notifier).regenerateInvitationCode();
+        showSnackBarWithText(context, '招待コードを更新しました。');
+      },
+      label: const Text('更新する'),
+    );
+  }
+}
